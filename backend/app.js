2bac41fbc4d5de55e705a3f7134d6fc1bc9ff6bb
Andreas Thiberg <andreas.thiberg@gmail.com>
2022-11-27 20:31:22 +0100

    Installed GraphQL

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 /* Import packages */
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { GraphQLSchema } = require("graphql");
 const mysql = require("mysql");
 const dbConfig = require("./config/db.config");
 require("dotenv").config();
@@ -10,6 +12,19 @@ const port = process.env.NODE_DOCKER_PORT || 3000;
 
 /* Create db connection */
 const connection = mysql.createPool(dbConfig);
+
+/* Setup GraphQL route */
+
+const RootQueryType = require("./graphql/root.js");
+
+const schema = new GraphQLSchema({
+    query: RootQueryType
+});
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true,
+}));
   
 /* Index route */
 app.get("/", (req, res) => {
