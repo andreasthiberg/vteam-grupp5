6c2f395ec6c42af8013abbd92df0210a7b0e5f63
Tomoko Ishii <tomoko.s.ishii@gmail.com>
2022-11-29 13:22:09 +0100

    Setup React&Docker for webapp_admin&webapp_kund

---
+++ webapp_admin/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>High5 Elsparkcyklar</h1>
+        <p>This website is for admin!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ webapp_kund/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>High5 Elsparkcyklar</h1>
+        <p>This website is for customers!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
