dfce0937550822be9fa7b7a624a97cb0cb749a85
Tomoko Ishii <tomoko.s.ishii@gmail.com>
2022-12-02 12:32:49 +0100

    Added mobilapp

---
+++ mobilapp/App.tsx
@@ -0,0 +1,41 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+import { Base } from './styles';
+import Home from './components/Home';
+import Map from './components/Map';
+
+const Tab = createBottomTabNavigator();
+const routeIcons = {
+  "Home": "home",
+  "Map": "map",
+}
+
+export default function App() {
+
+  return (
+    <SafeAreaView style={Base.container}>
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+                let iconName = routeIcons[route.name] || "alert";
+                return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'blue',
+            tabBarInactiveTintColor: 'gray',
+          })}
+        >
+        
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Map" component={Map} />
+
+        </Tab.Navigator>
+      </NavigationContainer>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
