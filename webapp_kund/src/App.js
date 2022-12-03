d2dae8a73db53365897ccbbbea42b5e011ce62bc
Tomoko Ishii <tomoko.s.ishii@gmail.com>
2022-12-03 09:59:08 +0100

    Added navigation

--- webapp_kund/src/App.js
+++ webapp_kund/src/App.js
@@ -1,13 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Home from './pages/Home';
+import History from './pages/History';
+import Payment from './pages/Payment';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>High5 Elsparkcyklar</h1>
-        <p>This website is for customers!</p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+      </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/history' element={<History />} />
+        <Route path='/payment' element={<Payment />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ webapp_kund/src/components/Header.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <>
+            <h1>High5 Customer's Page</h1>
+            <ul className="nav">
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/history'>History</Link>
+                </li>
+                <li>
+                    <Link to='/payment'>Payment</Link>
+                </li>
+            </ul>
+        </>
+    );
+}
+
+export default Header;
