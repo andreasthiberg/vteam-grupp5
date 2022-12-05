a99d67f247650141c6c6e46ddbdf900a4e1e1225
Andreas Thiberg <andreas.thiberg@gmail.com>
2022-12-05 18:08:04 +0100

    Initialized Oauth rotues in backend

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,13 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const { GraphQLSchema } = require('graphql')
+const cors = require('cors')
 require('dotenv').config()
 
 // Setup express server
 const app = express()
 const port = process.env.NODE_DOCKER_PORT || 3000
+
 
 // Setup GraphQL route with schema
 const RootQueryType = require('./graphql/root_query.js')
@@ -19,16 +21,31 @@ const schema = new GraphQLSchema({
   mutation: RootMutationType
 })
 
+
+app.use(express.json());
+
+
+app.use(cors({
+  origin: '*'
+}));
+
+
 // Add GraphQL route
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }))
 
+// Oauth router
+const authRouter = require('./routes/auth');
+app.use('/auth', authRouter);
+
 // Index route
 app.get('/', (req, res) => {
   res.json('Welcome to the High5 GraphQL API.')
 })
+
+
 
 // Start server
 app.listen(port, () => console.log('Listening on port ' + port))
