22fab5f753f1381ea5c206800fd59e50b64713c0
Andreas Thiberg <andreas.thiberg@gmail.com>
2022-12-05 12:07:59 +0100

    Initial Oauth2 implementation

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,31 @@
-// Express server setup
+// Express server setup with GraphQL and Oauth
 
 // Import packages
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const { GraphQLSchema } = require('graphql')
+var bodyParser = require('body-parser');
+var OAuthServer = require('express-oauth-server');
 require('dotenv').config()
 
 // Setup express server
 const app = express()
 const port = process.env.NODE_DOCKER_PORT || 3000
+
+// OAuth implementation
+app.oauth = new OAuthServer({
+  model: {},
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(app.oauth.authorize());
+
+const Request = OAuthServer.Request;
+const Response = OAuthServer.Response;
+
+let request = new Request({/*...*/}); 
+let response = new Response({/*...*/});
+
 
 // Setup GraphQL route with schema
 const RootQueryType = require('./graphql/root_query.js')
