2d7b89af890d8b0fb396dd8f7ef206c554b0d36f
Tomoko Ishii <tomoko.s.ishii@gmail.com>
2022-12-07 13:32:52 +0100

    bug

--- webapp_admin/src/pages/Customers.js
+++ webapp_admin/src/pages/Customers.js
@@ -1,9 +1,5 @@
 const Customers = () => {
-<<<<<<< HEAD
     return <h1>Customer List Page</h1>;
-=======
-    return <h1>Customer information Page</h1>;
->>>>>>> main
 };
 
 export default Customers;
