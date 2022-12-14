c712ba1ac5a7b1031b54871bd83b81d80b6a19f4
Maal21maat <maat21@student.bth.se>
2022-12-14 17:29:43 +0000

    models for connect db

---
+++ webapp_admin/src/models/customers.js
@@ -0,0 +1,33 @@
+// Fetch customer's info from graphql
+
+const customers = {
+    // baseUrl:
+
+    getAllCustomers: async function getAllCustomers() {
+        const query = `
+            query {
+                customers {
+                    first_name
+                    last_name
+                }
+            }
+        `;
+
+        const response = await fetch('http://localhost:3000/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({
+                query
+            })
+        });
+
+        const result = await response.json();
+        
+        return result.data;
+    }
+};
+
+export default customers;
