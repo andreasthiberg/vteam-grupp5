700c5c2d9321c99d04953ba035b52d30c85e94ad
Andreas Thiberg <andreas.thiberg@gmail.com>
2022-12-17 15:39:42 +0100

    Temporarily disabled zoneCalculation

--- backend/models/statusCalc.js
+++ backend/models/statusCalc.js
@@ -6,12 +6,14 @@ const statusCalc = {
   // Check if coordinates are in parking/charging zone at the end of trip
   zoneCalculation: async function zoneCalculation (cityId,scooterCoords) {
 
-    let parkingZones = mapModel.getParkingZones();
-    let chargingStations = mapModel.getChargingStations();
+    // let parkingZones = mapModel.getParkingZones();
+    // let chargingStations = mapModel.getChargingStations();
 
     positionCode = 0 // 0 = Utanför zoner, 1 = Parkeringszon, 2 = Laddningszon
+
     // Undersök om koordinater är i zon
     // Sätt positionCode till relevant siffra.
+    
     return positionCode
   },
   findClosestChargingStation: async function findClosestChargingStation(cityId,scooterCoords) {
