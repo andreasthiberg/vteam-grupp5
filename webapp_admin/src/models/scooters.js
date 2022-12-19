f830937076a0c65cbfa3e5532085f01ab04c4292
Maal21maat <maat21@student.bth.se>
2022-12-19 19:16:35 +0000

    added scooter list

---
+++ webapp_admin/src/models/scooters.js
@@ -0,0 +1,35 @@
+// Fetch customer's info from graphql
+
+const scooters = {
+    // baseUrl:
+
+    getAllScooters: async function getAllScooters() {
+        const query = `
+            query {
+                scooters {
+                    id
+                    pos
+                    status
+                    battery
+                }
+            }
+        `;
+
+        const response = await fetch('http://localhost:3000/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({
+                query
+            })
+        });
+
+        const result = await response.json();
+        
+        return result.data;
+    }
+};
+
+export default scooters;
--- webapp_admin/src/pages/Scooters.js
+++ webapp_admin/src/pages/Scooters.js
@@ -1,11 +1,28 @@
 import { React } from 'react-router-dom';
-function Scooters() {
-  return (
+import { useState, useEffect } from 'react';
+import scooterModel from '../models/scooters';
+
+
+export default function Scooters() {
+    const [scootersInfo, setScootersInfo] = useState([]);
+  
+    useEffect(() => {
+      (async () => {
+        const response = await scooterModel.getAllScooters();
+  
+        setScootersInfo(response.scooters);
+  
+        })();
+    }, []);
+  
+    let list = scootersInfo.map((scooter, index) => <li key={index}>{scooter.id}{scooter.pos}{scooter.status}{scooter.battery}</li>);
+
+    return (
     <div>
-      <p>Hej, här skulle egentligen valet för varje stad ligga, men nu belv det inte så. Går utmärkt att ändra om man har en lösning på det.</p>
+        <h1>Scooters List Page</h1>
+        <p>Egentligen vill jag ha en knapp/rullist med städer och sen kommer kartan upp, men fn ligger de på egen sida</p>
+        <h2>List of scooters</h2>
+        {list}
     </div>
-
-  );
+    );
 }
-
-export default Scooters;
