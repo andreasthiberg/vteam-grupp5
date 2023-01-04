c52c007a2cbf4d172ed8276fdc8d06bcbf00d976
Marlena Bazan <marlena.bazan@hotmail.com>
2023-01-04 19:57:32 +0100

    Fixed scooter insert file

--- webapp_admin/src/pages/Scooters.js
+++ webapp_admin/src/pages/Scooters.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import { Marker, Popup, TileLayer, MapContainer } from "react-leaflet";
-import "../App.css";
-import MapLun from "../components/MapLun";
-import MapSto from "../components/MapSto";
-import MapMal from "../components/MapMal";
-
-
-function Scooters () {
-
-
-  return (
-    <div>
-      <p>Här skall valen göras för de olika städer</p>
-      <h2>Stockholm</h2>
-      <MapSto />
-    </div>
-  )
-}
-
-export default { Scooters };
-=======
 import { React } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import scooterModel from '../models/scooters';
