1c6297bc12d5c0ad9a383ae4faa66c8d5d6a3e6b
Maal21maat <maat21@student.bth.se>
2023-01-05 10:52:16 +0000

    added list to scooter and customer

--- webapp_admin/src/pages/Customers.js
+++ webapp_admin/src/pages/Customers.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import customerModel from '../models/customers';
-import CustomerCard from '../components/CustomerCard';
+
 
 
 export default function Customers() {
@@ -14,20 +14,46 @@ export default function Customers() {
   
         })();
     }, []);
-  
-    // let list = customersInfo.map((name, index) => <li key={index}>{name.first_name}{name.last_name}</li>);
-    let customerlist = customersInfo.map((customer, index) => {
-        return <CustomerCard
-                    customer={customer}
-                    key={index}
-                />;
-    });
 
     return (
     <div>
         <h1>Customer List Page</h1>
-        <div className='list'>{customerlist}</div>
+        <div className = "row">
+        <button className="btn btn-primary"> Add Customer</button>
+        </div>
+        <br></br>
+            <div className = "row">
+                <table className = "table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th> Id</th>
+                            <th> Customer name</th>
+                            <th> Customer mail</th>
+                            <th> Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            customersInfo.map(
+                                customer => 
+                                <tr key = {customer.id}>
+                                        <td> {customer.id} </td>
+                                        <td> {customer.last_name} {customer.first_name}</td>
+                                        <td> {customer.email}</td>
+                                        <td>
+                                            <button className="btn btn-info">Update </button>
+                                            <button className="btn btn-danger">Delete </button>
+                                            <button className="btn btn-info">View </button>
+                                        </td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </table>
+            </div>
     </div>
     );
+
+
 }
 
