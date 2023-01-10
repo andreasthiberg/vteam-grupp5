fd374e6f4de2c8465abfe89033bc80019b69fccd
Marlena Bazan <marlena.bazan@hotmail.com>
2023-01-10 17:20:19 +0100

    zone calculation function

--- webapp_admin/src/components/CustomerInfo.js
+++ webapp_admin/src/components/CustomerInfo.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-export default function customerInfo({setSelectedCustomer,selectedCustomer,tripsInfo}) {
+export default function CustomerInfo({setSelectedCustomer,selectedCustomer,tripsInfo}) {
 
     const [matchingTrips,setMatchingTrips] = useState([]);
 
